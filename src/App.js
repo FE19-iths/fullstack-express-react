2e37a4671b4ef978c6bb670b739d2a1c8168c83f
David Andersson <lejonmanen@gmail.com>
2020-06-10 09:44:11 +0200

    Added backend project

--- src/App.js
+++ src/App.js
@@ -1,26 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <h1> Fullstack demo </h1>
+            </header>
+            <main>
+                <h2> Demonstrera API </h2>
+                <p>Här kommer vi göra fetch om en stund...</p>
+            </main>
+        </div>
+    );
 }
 
 export default App;
